f117a0ce386ccdc34a2d9f6cb2db52395cfa7202
dhouse1UNCC <dhouse1@uncc.edu>
2021-10-07 13:08:13 -0400

    Project Started "Pot-T"

---
+++ itis3135/project/scripts/gallery.js
@@ -0,0 +1,9 @@
+//[id, image_name, description]
+var imageinfo=[];
+
+/* Takes care of processes that need to take place on page start up, such as filling the array above.
+
+ */
+function pageStartup(){
+	
+}
