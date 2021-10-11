1859d2a8d105451b973bbaa9718edca0e2ed8ef6
dhouse1UNCC <dhouse1@uncc.edu>
2021-10-10 20:10:16 -0400

    Activity 8 complete

--- itis3135/scripts/salary.js
+++ itis3135/scripts/salary.js
@@ -1,49 +1,70 @@
-var person = [];
-var salary = [];
+var person = [["Daniel H.", 0], ["Mel B.", 0], ["Darien J.", 0], ["Justin D.", 0]];
 
+//Adds a salary to the table
 function addSalary(){
-	money = document.getElementById("salary").value;
-	name = document.getElementById("name").value;
-	if(name =="" || isNaN(money)){
+	var money = parseFloat(document.getElementById("salary").value);
+	var name = document.getElementById("names").value;
+	if(isNaN(money) || parseFloat(money).toFixed(2) <=	0.00){
 		alert("Looks like you typed something in wrong! Please validate your enties and try again.");
 		return;
 	}
 	money = parseFloat(money).toFixed(2);
-	salary.push(money);
-	person.push(name);
-	document.getElementById("name").value = "";
+	for(var i = 0; i < person.length; i++){
+		if(name === person[i][0]){
+			person[i][1] = money;
+		} 
+	}
 	document.getElementById("salary").value= 0;
-	document.getElementById("name").focus();
+	document.getElementById("names").focus();
 }
 
+//Displays average and highest salary
 function displayResults(){
-	if(salary.length==0){
+	if(person.length==0){
 		alert("Please enter some values before trying to find an average!");
 		return;
 	}
 	var average = parseFloat(0);
 	var highest = 0;
-	for(var i = 0; i < salary.length; i++){
-		if(salary[i] > parseFloat(highest)){
-			highest = salary[i];
+	for(var i = 0; i < person.length; i++){
+		if(person[i][i] > parseFloat(highest)){
+			highest = person[i][1];
 		}
-		average= parseFloat(average) + parseFloat(salary[i]);
+		average= parseFloat(average) + parseFloat(person[i][1]);
 	}
-	average= parseFloat(average)/parseFloat(salary.length);
+	average= parseFloat(average)/parseFloat(person.length);
 	average = average.toFixed(2);
+	highest = highest.toFixed(2);
 	var results = "<h2>Highest Salary || Average Salary</h2><p>$" + String(highest) + " || $" + String(average) + "</p>";
 	document.getElementById("results").innerHTML = results;
 }
 
+//Displays salaries in a table
 function displaySalaries(){
-	if(salary.length==0){
+	if(person.length==0){
 		alert("Please enter some values before generating a table!");
 		return;
 	}
 	var results = "<table><thead><tr><th>Name</th><th>Salary</th></tr></thead><tbody>";
-		for(var i = 0; i < salary.length; i++){
-			results+="<tr><td>" + String(person[i]) + "</td><td>$" + String(salary[i]) + "</td></tr>";
+		for(var i = 0; i < person.length; i++){
+			results+="<tr><td>" + String(person[i][0]) + "</td><td>$" + String(person[i][1]) + "</td></tr>";
 		}
 	results += "</table>";
 	document.getElementById("results_table").innerHTML = results;
+}
+
+//A function to fill the dropbox with the information of the persons table's 1st column.
+function fillDropbox(){
+	//alert(person[0][0]);
+	var choices = "";
+	var full_name = "";
+  var seperated_name = [];
+	var current_id = "";
+	for(var i = 0; i < person.length; i++){
+		full_name=person[i][0];
+		seperated_name= full_name.split(" ");
+		current_id = seperated_name[0].toLowerCase();
+		choices += "<option>" + person[i][0] +"</option>";
+	}
+	document.getElementById("names").innerHTML = choices;
 }
