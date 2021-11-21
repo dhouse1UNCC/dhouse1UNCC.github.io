0a24b74d00ea3d58e859c0df78422e7b209b7e1b
dhouse1UNCC <dhouse1@uncc.edu>
2021-11-20 22:12:53 -0500

    Activity 14 Upload

---
+++ itis3135/activities/Activity14/Activity14.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+    var jsonName= '';
+	
+	$("a").click(function(event){
+		event.preventDefault();
+		jsonName = $(this).attr("title");
+		$.ajax({
+			type: "get",
+			url: jsonName + ".json",
+			beforeSend: function() {
+				$("#team").html("Loading...");
+			},
+			timeout: 10000,
+			error: function(xhr, status, error) {
+				alert("Error: " + xhr.status + " - " + error);
+			},
+			dataType: "json",
+			success: function(data) {
+				$("main").html("");
+				$.each(data, function() {
+					$.each(this, function(key, value) {
+						$("main").append(
+							"<h1>" + value.title + "</h1>" +
+							"<h2>" + value.month + "</h2>" +
+							"<h3>" + value.speaker + "</h3>" +
+							"<img src='" + value.image + "'>" +
+							"<p>"+ value.text +"</p>"
+						);
+					});
+				});
+			}
+		});
+	});
+	
+
+});
