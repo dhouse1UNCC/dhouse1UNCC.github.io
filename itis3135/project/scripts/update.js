e6889fff7bd53a07cda1e50cf0b324a0241eabe7
dhouse1UNCC <dhouse1@uncc.edu>
2021-11-20 21:54:25 -0500

    Update Page Created

--- itis3135/project/scripts/update.js
+++ itis3135/project/scripts/update.js
@@ -2,7 +2,8 @@ $(document).ready(function(){
 	var comp = document.getElementById('update_comparison');
 	var pic = document.getElementById('update_picture');
 	var journ = document.getElementById('update_journal');
-	alert();
+	var pro_count = 1;
+	var con_count = 1;
 	$('#comparison_button').click(function(){
 		if(pic.style.display !== 'none'){
 			pic.style.display = 'none';
@@ -29,5 +30,49 @@ $(document).ready(function(){
 			comp.style.display = 'none';
 		}
 		journ.style.display = 'block';
+	});
+	$('#add_pro').click(function(){
+		var id_check = 'pro' + (pro_count-1);
+		var lastElement = document.getElementById(id_check);
+		if(lastElement.value==null){
+			alert('Enter a pro in the available spot before trying to add another one!');
+			return;
+		}
+		if(lastElement.value.trim()=='' || lastElement.value.trim()=='None' || lastElement.value.trim()=='Pro'){
+			alert('Enter a pro in the available spot before trying to add another one!');
+			return;
+		}
+		var ul = document.getElementById('comparison_pro');
+		var li = document.createElement('li');
+		var textbox = document.createElement('input');
+		textbox.setAttribute('type', 'text');
+		textbox.setAttribute('value', 'Pro');
+		var id = "pro" + pro_count;
+		pro_count++;
+		textbox.setAttribute("id", id);
+		li.append(textbox);
+		ul.appendChild(li);
+	});
+	$('#add_con').click(function(){
+		var id_check = 'con' + (con_count-1);
+		var lastElement = document.getElementById(id_check);
+		if(lastElement.value==null){
+			alert('Enter a con in the available spot before trying to add another one!');
+			return;
+		}
+		if(lastElement.value.trim()=='' || lastElement.value.trim()=='None' || lastElement.value.trim()=='Conk'){
+			alert('Enter a con in the available spot before trying to add another one!');
+			return;
+		}
+		var ul = document.getElementById('comparison_con');
+		var li = document.createElement('li');
+		var textbox = document.createElement('input');
+		textbox.setAttribute('type', 'text');
+		textbox.setAttribute('value', 'Con');
+		var id = "con" + con_count;
+		con_count++;
+		textbox.setAttribute("id", id);
+		li.append(textbox);
+		ul.appendChild(li);
 	});
 });
