e5c97a3e74cc4d7b54270085f0f6433900d3fa9f
dhouse1UNCC <dhouse1@uncc.edu>
2021-12-05 15:19:09 -0500

    Update journal.js
    Documentation

--- itis3135/project/scripts/journal.js
+++ itis3135/project/scripts/journal.js
@@ -16,20 +16,27 @@ $(document).ready(function() {
 		success: function(data) {
 			$("#journal").html("");
 			$.each(data, function() {
+				//Code to to ensure proper entry correlation with the slider.
+				//Oldest entries are at the top of JSON file and the newer entries are added on top of the older
+				//entries in order
 				$.each(this, function(key, value) {
 					var id="entry" + entryCount;
 					var htmlCode = '<div class="entry" id="entry' + entryCount +'" tabindex="' + entryCount + '">'+ 
 						'<h3>' + value.date + '</h3>' + '<h3>' + value.title + 
 						'</h3>' + '<div class ="jline"><div></div></div>' + 
 						'<p>' + value.content+ '</p>' + '</div>';
+					//If this is no the 1st entry, add the other entries below the current one.
 					if(entryCount != 0){
 						htmlCode += $("#journal").html();
 						}
+					//Update journal Html
 					$("#journal").html(htmlCode);
+					//Count that another entry has been added
 					entryCount++;
 				});
 			});
 		},
+		//On complete create the journal slider
 		complete:function(data){
 			$( "#slider").slider({
 				orientation: "vertical",
@@ -38,6 +45,7 @@ $(document).ready(function() {
 				max: entryCount-1,
 				value:  entryCount-1,
 				setp: 1,
+				//When the slider is changed the according entry is focused and highlighted
 				slide: function( event, ui ) {
 					var idName= "entry" + ui.value;
 					document.getElementById(idName).focus();
